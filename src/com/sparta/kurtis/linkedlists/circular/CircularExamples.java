a7c925d8ffc5a821133ba09e3c73308c234a81b6
DBZMW98 <wahdelmw@gmail.com>
2021-02-07 18:09:15 +0000

    Implemented addFirst, addLast, removeFirst and removeLast

--- src/com/sparta/kurtis/linkedlists/circular/CircularExamples.java
+++ src/com/sparta/kurtis/linkedlists/circular/CircularExamples.java
@@ -25,8 +25,8 @@ public class CircularExamples extends Example {
         String monopolySpace12 = "Liverpool Street Station";
         String monopolySpace13 = "Mayfair";
 
-        monopolyBoard.add(monopolySpace1);
-        monopolyBoard.add(monopolySpace2);
+        monopolyBoard.addFirst(monopolySpace2);
+        monopolyBoard.addFirst(monopolySpace1);
         monopolyBoard.add(monopolySpace3);
         monopolyBoard.add(monopolySpace4);
         monopolyBoard.add(monopolySpace5);
@@ -38,11 +38,14 @@ public class CircularExamples extends Example {
         monopolyBoard.add(monopolySpace11);
         monopolyBoard.add(monopolySpace12);
         monopolyBoard.add(monopolySpace13);
+//        monopolyBoard.add(2, monopolySpace2);
 
         Printer.printStringCircularLinkedList(monopolyBoard, 2);
         Printer.printMessage("Size: " + monopolyBoard.size());
 
-        Printer.printMessage("Removed: " + monopolyBoard.remove());
+        Printer.printMessage("Removed: " + monopolyBoard.removeLast());
+        Printer.printStringCircularLinkedList(monopolyBoard, 1);
+        Printer.printMessage("Size: " + monopolyBoard.size());
         Printer.printMessage("Removed: " + monopolyBoard.remove());
         Printer.printMessage("Removed: " + monopolyBoard.remove());
         Printer.printMessage("Removed: " + monopolyBoard.remove());
--- src/com/sparta/kurtis/linkedlists/circular/CircularLinkedList.java
+++ src/com/sparta/kurtis/linkedlists/circular/CircularLinkedList.java
@@ -22,9 +22,50 @@ public class CircularLinkedList extends LinkedList {
     }
 
     @Override
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (this.getHead() == null) {
+            setTail(newNode);
+
+        } else {
+            newNode.next = getHead();
+        }
+        setHead(newNode);
+        getTail().next = getHead();
+    }
+
+    @Override
+    public void addLast(String data) {
+        this.add(data);
+    }
+
+    @Override
     public String remove() {
         String removed = null;
-        if (size() < 1) {
+        if (getHead() == null) {
+            Printer.printErrorMessage("List is empty");
+        } else {
+            removed = getHead().data;
+            if (getHead() == getTail()) {
+                setHead(null);
+                setTail(null);
+            } else {
+                setHead(getHead().next);
+                getTail().next = getHead();
+            }
+        }
+        return removed;
+    }
+
+    @Override
+    public String removeFirst() {
+        return remove();
+    }
+
+    @Override
+    public String removeLast() {
+        String removed = null;
+        if (getHead() == null) {
             Printer.printErrorMessage("List is empty");
         } else {
             if (getHead() == getTail()) {
@@ -50,8 +91,6 @@ public class CircularLinkedList extends LinkedList {
         Node current = getHead();
         if (getHead() == null && getTail() == null) {
             return count;
-//        } else if (getHead() == getTail()) {
-//            count = 1;
         } else {
             do {
                 count++;
